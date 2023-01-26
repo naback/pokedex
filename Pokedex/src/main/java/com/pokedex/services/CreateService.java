d697dff0ceff31b181fdcda6ae3cf3b8f628cc9e
king naback <naback@live.com>
2023-01-26 20:11:14 -0300

    logs inserted and exception usage changed to standard exception class

--- Pokedex/src/main/java/com/pokedex/services/CreateService.java
+++ Pokedex/src/main/java/com/pokedex/services/CreateService.java
@@ -1,30 +1,36 @@
 package com.pokedex.services;
 
-import com.fasterxml.jackson.databind.ser.Serializers;
 import com.pokedex.db.Pokemon;
 import com.pokedex.db.PokemonRepository;
 import com.pokedex.mappers.PokemonMapper;
 import com.pokedex.model.BaseResponse;
 import com.pokedex.model.CreateRequest;
-import com.pokedex.model.MissingParameterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateService extends BaseService {
+
+    public static final Logger logger = LoggerFactory.getLogger(CreateService.class);
     @Autowired
     private PokemonRepository pokemonRepository;
 
     public ResponseEntity create(CreateRequest request) {
         try {
+            logger.info("starting create service");
+
             checkCreateRequestData(request);
             Pokemon pokemon = mapFromRequestToDbObject(request);
             pokemonRepository.save(pokemon);
+
+            logger.info("create service finished with success");
             return ResponseEntity.status(200).build();
         }
-        catch (MissingParameterException me) {
-            return ResponseEntity.status(400).body(new BaseResponse(me.getErrorMessage()));
+        catch (Exception e) {
+            return ResponseEntity.status(400).body(new BaseResponse(e.getMessage()));
         }
     }
 
@@ -32,21 +38,21 @@ public class CreateService extends BaseService {
         return PokemonMapper.INSTANCE.createRequestToPokemon(request);
     }
 
-    private void checkCreateRequestData(CreateRequest request) throws MissingParameterException {
+    private void checkCreateRequestData(CreateRequest request) throws Exception {
         if (nullOrEmpty(request.getName())) {
-            throw new MissingParameterException("Parameter name is null! It can't be!");
+            throw new Exception("Parameter name is null! It can't be!");
         }
 
         if (request.getAttack() == null) {
-            throw new MissingParameterException("Parameter attack is null! It can't be!");
+            throw new Exception("Parameter attack is null! It can't be!");
         }
 
         if (request.getDefense() == null) {
-            throw new MissingParameterException("Parameter defense is null! It can't be!");
+            throw new Exception("Parameter defense is null! It can't be!");
         }
 
         if (request.getStamina() == null) {
-            throw new MissingParameterException("Parameter stamina is null! It can't be!");
+            throw new Exception("Parameter stamina is null! It can't be!");
         }
     }
 }
