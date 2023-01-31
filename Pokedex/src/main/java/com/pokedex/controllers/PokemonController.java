33e3d9a168eba47599f56b6abb8cb9abb10d7171
king naback <naback@live.com>
2023-01-30 21:10:39 -0300

    list service implemented

--- Pokedex/src/main/java/com/pokedex/controllers/PokemonController.java
+++ Pokedex/src/main/java/com/pokedex/controllers/PokemonController.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pokemon")
@@ -40,5 +37,10 @@ public class PokemonController {
     @PostMapping("/delete")
     private ResponseEntity deletePokemon(@RequestBody DeleteRequest request) {
         return deleteService.delete(request);
+    }
+
+    @GetMapping("/list")
+    private ResponseEntity listPokemon() {
+        return listService.list();
     }
 }
--- Pokedex/src/main/java/com/pokedex/services/ListService.java
+++ Pokedex/src/main/java/com/pokedex/services/ListService.java
@@ -1,7 +1,33 @@
 package com.pokedex.services;
 
+import com.pokedex.db.PokemonRepository;
+import com.pokedex.model.BaseResponse;
+import com.pokedex.model.DeleteRequest;
+import com.pokedex.model.ListResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ListService {
+    public static final Logger logger = LoggerFactory.getLogger(ListService.class);
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    public ResponseEntity list() {
+        try {
+            logger.info("starting list service");
+
+            var allPokemons = pokemonRepository.findAll();
+
+            logger.info("list service finished with success");
+            return ResponseEntity.status(200).body(new ListResponse(allPokemons));
+        }
+        catch (Exception e) {
+            logger.error(e.getMessage());
+            return ResponseEntity.status(400).body(new BaseResponse(e.getMessage()));
+        }
+    }
 }
